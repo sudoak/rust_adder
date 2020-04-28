0e730313fba3368e0bbc02d527d7d81609823ab8
sudoak <sudoakk@gmail.com>
2020-04-28 22:05:35 +0530

    [test] : test to find if large rect fits in small rect

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    // #[should_panic(expected = "I expected error")]    
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 10,
+        };
+
+        assert!(larger.can_hold(&smaller), "Cant hold us");
     }
 }
